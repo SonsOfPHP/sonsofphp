592fe3051f855d9d915863689002b8a8263eb9ee
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2022-04-24 21:15:54 -0400

    [Money] Various updates

--- src/SonsOfPHP/Component/Money/Money.php
+++ src/SonsOfPHP/Component/Money/Money.php
@@ -11,14 +11,14 @@ namespace SonsOfPHP\Component\Money;
  */
 class Money implements MoneyInterface
 {
-    private $value;
+    private $amount;
     private CurrencyInterface $currency;
 
     /**
      */
-    public function __construct($value, CurrencyInterface $currency)
+    public function __construct($amount, CurrencyInterface $currency)
     {
-        $this->value    = $value;
+        $this->amount   = $amount;
         $this->currency = $currency;
     }
 
@@ -27,23 +27,103 @@ class Money implements MoneyInterface
      */
     public function __toString(): string
     {
-        return $this->value.' '.$this->currency->getCurrencyCode();
+        return $this->amount.' '.$this->currency->getCurrencyCode();
     }
 
-    public function getValue()
+    /**
+     * @return string
+     */
+    public function getAmount()
     {
-        return $this->value;
+        return $this->amount;
     }
 
+    /**
+     * @return CurrencyInterface
+     */
     public function getCurrency(): CurrencyInterface
     {
         return $this->currency;
     }
 
     /**
+     * @return bool
      */
     public function equals(MoneyInterface $that): bool
     {
-        return $this->getValue() === $that->getValue() && $this->getCurrency()->equals($that->getCurrency());
+        return $this->getAmount() === $that->getAmount() && $this->getCurrency()->equals($that->getCurrency());
+    }
+
+    /**
+     * @param MoneyInterface $that
+     *
+     * @return int
+     *  -1 = this less than that
+     *   0 = this equals that
+     *   1 = this greater than that
+     */
+    public function compare(MoneyInterface $that): int
+    {
+        if ($this->getCurrency()->getCode() !== $that->getCurrency()->getCode()) {
+            throw new MoneyException('Cannot Compare different curriences');
+        }
+
+        if ($this->getAmount() < $that->getAmount()) {
+            return -1;
+        }
+
+        if ($this->getAmount() > $that->getAmount()) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    /**
+     * @todo
+     * Adds amount to existing amount and returns a new MoneyInterface
+     *
+     * @param MoneyInterface $money
+     *
+     * @return static
+     */
+    public function add(MoneyInterface $money)
+    {
+    }
+
+    /**
+     * @todo
+     * Subtracts amount to existing amount and returns a new MoneyInterface
+     *
+     * @param MoneyInterface $money
+     *
+     * @return static
+     */
+    public function subtract(MoneyInterface $money)
+    {
+    }
+
+    /**
+     * @todo
+     * Multiple by multiplier and returns new MoneyInterface
+     *
+     * @param int|string
+     *
+     * @return static
+     */
+    public function multiply($multiplier)
+    {
+    }
+
+    /**
+     * @todo
+     * Divide by divisor and returns new MoneyInterface
+     *
+     * @param int|string
+     *
+     * @return static
+     */
+    public function divide($divisor)
+    {
     }
 }
