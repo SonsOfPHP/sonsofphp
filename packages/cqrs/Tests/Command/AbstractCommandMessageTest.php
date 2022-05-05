345e6aa16a9debcceb73ea12284e4c5b70291c4f
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2022-05-05 18:40:46 -0400

    [CQRS] Adding a few tests

---
+++ packages/cqrs/Tests/Command/AbstractCommandMessageTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SonsOfPHP\Component\Cqrs\Tests\Command;
+
+use PHPUnit\Framework\TestCase;
+use SonsOfPHP\Component\Cqrs\Tests\DummyCommand;
+use SonsOfPHP\Component\Cqrs\Command\AbstractCommandMessage;
+use SonsOfPHP\Component\Cqrs\Command\CommandMessageInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author Joshua Estes <joshua@sonsofphp.com>
+ */
+final class AbstractCommandMessageTest extends TestCase
+{
+    public function testItHasTheCorrectInterface(): void
+    {
+        $command = $this->getMockForAbstractClass(AbstractCommandMessage::class);
+
+        $this->assertInstanceOf(CommandMessageInterface::class, $command);
+    }
+
+    public function testConstructor(): void
+    {
+        DummyCommand::setConfigureOptionsCallback(function ($resolver) {
+            $resolver->define('id');
+        });
+        $cmdOptions = [
+            'id' => 'unique-id',
+        ];
+        $command = new DummyCommand($cmdOptions);
+
+        $this->assertArrayHasKey('id', $command->getOptions());
+        $this->assertSame('unique-id', $command->getOption('id'));
+    }
+}
