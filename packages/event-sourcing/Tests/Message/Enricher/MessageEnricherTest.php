69d642eb3e987f78239c8e81063ebe250b3fe737
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2022-05-07 22:42:48 -0400

    [EventSourcing] Testing

---
+++ packages/event-sourcing/Tests/Message/Enricher/MessageEnricherTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SonsOfPHP\Component\EventSourcing\Tests\Message;
+
+use SonsOfPHP\Component\EventSourcing\Exception\EventSourcingException;
+use SonsOfPHP\Component\EventSourcing\Message\Enricher\Handler\NullMessageEnricherHandler;
+use SonsOfPHP\Component\EventSourcing\Message\Enricher\Provider\AllMessageEnricherProvider;
+use SonsOfPHP\Component\EventSourcing\Message\Enricher\Provider\NullMessageEnricherProvider;
+use SonsOfPHP\Component\EventSourcing\Message\Enricher\MessageEnricherInterface;
+use SonsOfPHP\Component\EventSourcing\Message\Enricher\MessageEnricher;
+use SonsOfPHP\Component\EventSourcing\Message\MessageInterface;
+use SonsOfPHP\Component\EventSourcing\Metadata;
+use PHPUnit\Framework\TestCase;
+
+final class MessageEnricherTest extends TestCase
+{
+    public function testItHasTheRightInterface(): void
+    {
+        $enricher = new MessageEnricher(new NullMessageEnricherProvider());
+        $this->assertInstanceOf(MessageEnricherInterface::class, $enricher); // @phpstan-ignore-line
+    }
+
+    public function testItWillReturnMessageUntouchedWithNoHandlers(): void
+    {
+        $enricher        = new MessageEnricher(new NullMessageEnricherProvider());
+        $message         = $this->createMock(MessageInterface::class);
+        $enrichedMessage = $enricher->enrich($message);
+
+        $this->assertSame($enrichedMessage, $message);
+    }
+
+    public function testItWillCanEnrichMessage(): void
+    {
+        $provider = new AllMessageEnricherProvider();
+        $provider->register(new NullMessageEnricherHandler());
+
+        $enricher        = new MessageEnricher($provider);
+        $message         = $this->createMock(MessageInterface::class);
+        $enrichedMessage = $enricher->enrich($message);
+
+        $this->assertSame($enrichedMessage, $message);
+    }
+}
