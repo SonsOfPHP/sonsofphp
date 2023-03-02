a4b66557dbf118d550ce5fb8a2ba9ed3ed585d4e
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-03-02 15:48:44 -0500

    php cs fixer dist

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,9 +11,22 @@ return $config->setRules([
         '@PHP81Migration' => true,
         '@Symfony' => true,
         '@Symfony:risky' => true,
+        '@PHPUnit84Migration:risky' => true,
+
+        // PHPUnit Rules
+        //'php_unit_internal_class' => ['types' => ['normal', 'final']],
+        //'php_unit_test_case_static_method_calls' => ['call_type' => 'this'],
+        //'php_unit_test_class_requires_covers' => true,
+
         // Rules
+        //'align_multiline_comment' => true,
+        //'phpdoc_line_span' => true,
+        //'phpdoc_order_by_value' => ['annotations' => ['covers', 'throws']],
         'phpdoc_order' => true,
-        //'php_unit_test_class_requires_covers' => true, // @todo
+        //'phpdoc_tag_casing' => true,
+        //'array_indentation' => true,
+        //'explicit_indirect_variable' => true,
+        //'binary_operator_spaces' => ['default' => 'align_single_space'],
         'phpdoc_to_comment' => false,
     ])
     ->setFinder($finder)
