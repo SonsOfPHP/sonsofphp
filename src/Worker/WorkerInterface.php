bda26596877cacb7da88976d87e55b30513eae7c
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-10-21 10:00:36 -0400

    Squashed 'src/SonsOfPHP/Bard/' content from commit adaee48
    git-subtree-dir: src/SonsOfPHP/Bard
    git-subtree-split: adaee48e670a47b526fbdb0631d3333cde046990

---
+++ src/Worker/WorkerInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SonsOfPHP\Bard\Worker;
+
+/**
+ * Use for various tasks.
+ *
+ * @author Joshua Estes <joshua@sonsofphp.com>
+ */
+interface WorkerInterface
+{
+    // public function apply();
+}
