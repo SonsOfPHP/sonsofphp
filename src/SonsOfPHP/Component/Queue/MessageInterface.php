ec3bca06bd71b256a0a4177dc896cbaf3f83c578
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-10-26 22:11:11 -0400

    Queue Component

---
+++ src/SonsOfPHP/Component/Queue/MessageInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SonsOfPHP\Component\Queue;
+
+/**
+ * @author Joshua Estes <joshua@sonsofphp.com>
+ */
+interface MessageInterface extends \Serializable, \JsonSerializable
+{
+}
---
+++ src/SonsOfPHP/Component/Queue/QueueInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SonsOfPHP\Component\Queue;
+
+/**
+ * @author Joshua Estes <joshua@sonsofphp.com>
+ */
+interface QueueInterface
+{
+    public function getName(): string;
+
+    public function publish(MessageInterface $message): void;
+
+    public function receive(): iterable;
+
+    public function delete(MessageInterface $message): void;
+
+    public function purge(): void;
+}
