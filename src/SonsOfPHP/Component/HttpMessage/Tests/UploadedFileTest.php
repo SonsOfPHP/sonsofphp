f9dd1568cc2fbb9df934a10ae6b25ef767751951
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-10-31 01:02:54 -0400

    Adding HttpMessage Component

---
+++ src/SonsOfPHP/Component/HttpMessage/Tests/UploadedFileTest.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SonsOfPHP\Component\HttpMessage\Tests;
+
+use PHPUnit\Framework\TestCase;
+use SonsOfPHP\Component\HttpMessage\UploadedFile;
+use SonsOfPHP\Component\HttpMessage\UploadedFileError;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * @coversDefaultClass \SonsOfPHP\Component\HttpMessage\UploadedFile
+ *
+ * @internal
+ */
+final class UploadedFileTest extends TestCase
+{
+    private $stream;
+
+    public function setUp(): void
+    {
+        $this->stream = $this->createMock(StreamInterface::class);
+        $this->stream->method('isReadable')->willReturn(true);
+    }
+
+    /**
+     * @coversNothing
+     */
+    public function testItImplementsCorrectInterface(): void
+    {
+        $this->assertInstanceOf(UploadedFileInterface::class, new UploadedFile($this->stream));
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testConstructWillThrowExceptionWhenStreamIsNull(): void
+    {
+        $this->expectException('InvalidArgumentException');
+        new UploadedFile(null);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testConstructWillThrowExceptionWhenErrorIsInvalid(): void
+    {
+        $this->expectException('InvalidArgumentException');
+        new UploadedFile(stream: $this->stream, error: 99999);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testConstructWillThrowExceptionWhenStreamIsUnreadable(): void
+    {
+        $stream = $this->createMock(StreamInterface::class);
+        $stream->method('isReadable')->willReturn(false);
+
+        $this->expectException('InvalidArgumentException');
+        new UploadedFile($stream);
+    }
+
+    /**
+     * @covers ::getStream
+     */
+    public function testGetStreamWorksAsExpected(): void
+    {
+        $file = new UploadedFile($this->stream);
+        $this->assertSame($this->stream, $file->getStream());
+    }
+
+    /**
+     * @covers ::getSize
+     */
+    public function testGetSizeWorksAsExpeected(): void
+    {
+        $file = new UploadedFile(
+            stream: $this->stream,
+            size: 2131,
+        );
+
+        $this->assertSame(2131, $file->getSize());
+    }
+
+    /**
+     * @covers ::getSize
+     */
+    public function testGetSizeWorksWhenSizeIsNotManuallySet(): void
+    {
+        $this->stream->method('getSize')->willReturn(2131);
+        $file = new UploadedFile($this->stream);
+        $this->assertSame(2131, $file->getSize());
+    }
+
+    /**
+     * @covers ::getError
+     */
+    public function testGetErrorWorksAsExpected(): void
+    {
+        $file = new UploadedFile($this->stream, error: UploadedFileError::CANT_WRITE->value);
+        $this->assertSame(UploadedFileError::CANT_WRITE->value, $file->getError());
+    }
+
+    /**
+     * @covers ::getClientFilename
+     */
+    public function testGetClientFilenameWorksAsExpected(): void
+    {
+        $file = new UploadedFile($this->stream, clientFilename: 'testing.txt');
+        $this->assertSame('testing.txt', $file->getClientFilename());
+    }
+
+    /**
+     * @covers ::getClientMediaType
+     */
+    public function testGetClientMediaTypeWorksAsExpected(): void
+    {
+        $file = new UploadedFile($this->stream, clientMediaType: 'media-type');
+        $this->assertSame('media-type', $file->getClientMediaType());
+    }
+}
---
+++ src/SonsOfPHP/Component/HttpMessage/UploadedFile.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SonsOfPHP\Component\HttpMessage;
+
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * {@inheritdoc}
+ *
+ * @author Joshua Estes <joshua@sonsofphp.com>
+ */
+class UploadedFile implements UploadedFileInterface
+{
+    public function __construct(
+        private ?StreamInterface $stream,
+        private ?int $size = null,
+        private int $error = \UPLOAD_ERR_OK,
+        private ?string $clientFilename = null,
+        private ?string $clientMediaType = null,
+    ) {
+        if (null === $stream || !$stream->isReadable()) {
+            throw new \InvalidArgumentException('Stream is invalid');
+        }
+
+        if (null === UploadedFileError::tryFrom($error)) {
+            throw new \InvalidArgumentException(sprintf('The value "%s" for $error is invalid.', $error));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getStream(): StreamInterface
+    {
+        if (null === $this->stream) {
+            throw new \RuntimeException('File has already been moved');
+        }
+
+        return $this->stream;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function moveTo(string $targetPath): void
+    {
+        // @todo throw new \InvalidArgumentExcpetion if targetPath is invalid
+
+        if (null === $this->stream) {
+            throw new \RuntimeException();
+        }
+
+        if (false === move_uploaded_file($this->stream->getMetadata('uri'), $targetPath)) {
+            throw new \RuntimeException('File could not be moved');
+        }
+
+        $this->stream = null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSize(): ?int
+    {
+        if (null !== $this->size) {
+            return $this->size;
+        }
+
+        if (null !== $this->stream) {
+            return $this->stream->getSize();
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getError(): int
+    {
+        return $this->error;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getClientFilename(): ?string
+    {
+        return $this->clientFilename;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getClientMediaType(): ?string
+    {
+        return $this->clientMediaType;
+    }
+}
