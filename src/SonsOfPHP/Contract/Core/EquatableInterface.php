a974c9b70895c9b8560b32ce7974a873082a0d2b
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-10-31 16:53:06 -0400

    more stuff

--- src/SonsOfPHP/Contract/Core/EquatableInterface.php
+++ src/SonsOfPHP/Contract/Core/EquatableInterface.php
@@ -13,14 +13,18 @@ namespace SonsOfPHP\Contract\Core;
 interface EquatableInterface
 {
     /**
-     * This needs to check to ensure both objects are equal to each other.
-     * Depending on how this is implemented, it can be loose (==) or strict
-     * (===).
+     * Checks object to see if it is equal to another object.
      *
      * @param mixed $other
+     * @param bool $strict
+     *   When true, this MUST compare objects using === and when false objects
+     *   MUST be compared using ==
+     *
+     * @throws \InvalidArgumentException
+     *   If $other is unexpected type
      *
      * @throws \RuntimeException
      *   When it cannot be determined if the objects are equal to each other
      */
-    public function equals($other, bool $strict = false): bool;
+    public function equals($other, bool $strict = true): bool;
 }
