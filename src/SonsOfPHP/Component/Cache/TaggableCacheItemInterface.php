428a1daa12a91480cf6018b8935e0411facfd281
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-11-07 13:10:15 -0500

    typos

--- src/SonsOfPHP/Component/Cache/TaggableCacheItemInterface.php
+++ src/SonsOfPHP/Component/Cache/TaggableCacheItemInterface.php
@@ -9,7 +9,17 @@ use Psr\Cache\CacheItemInterface;
 /**
  * @author Joshua Estes <joshua@sonsofphp.com>
  */
-interface TaggableCacheItem extends CacheItemInterface
+interface TaggableCacheItemInterface
 {
-    public function getTags(): iterable;
+    /**
+     * Returns the tags on the cache item
+     */
+    public function tags(): iterable;
+
+    /**
+     * Adds tags to the cache item
+     *
+     * $item->tag('test');
+     */
+    public function tag(string|iterable $tags): static;
 }
