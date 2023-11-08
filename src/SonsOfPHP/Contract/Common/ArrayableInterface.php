6902053eec8bcc1dd402f8e7c80d54313630eaad
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-11-08 14:17:00 -0500

    more common interfaces

---
+++ src/SonsOfPHP/Contract/Common/ArrayableInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SonsOfPHP\Contract\Common;
+
+/**
+ * @author Joshua Estes <joshua@sonsofphp.com>
+ */
+interface ArrayableInterface
+{
+    /**
+     */
+    public function toArray(): array;
+}
