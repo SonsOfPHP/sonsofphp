a9798895989005e6dd662d1aaa705a86c462d50d
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-12-08 16:30:11 -0500

    details

--- src/SonsOfPHP/Contract/Gateway/CreditCardInterface.php
+++ src/SonsOfPHP/Contract/Gateway/CreditCardInterface.php
@@ -9,4 +9,37 @@ namespace SonsOfPHP\Contract\Gateway;
  */
 interface CreditCardInterface
 {
+    /**
+     * name on card
+     */
+    public function getCardHolder(): string;
+
+    public function setCardHolder(string $cardHolder): self;
+
+    public function getCardNumber(): string;
+
+    public function setCardNumber(string $cardNumber): self;
+
+    /**
+     * Returns month in format MM
+     */
+    public function getExpirationMonth(): string;
+
+    /**
+     * Returns year in format YY
+     */
+    public function getExpirationYear(): string;
+
+    /**
+     * Month and Year SHOULD be in MM and YY format, however this should
+     * also be able to take a full year (2020) or just a single month (4)
+     */
+    public function setExpiration(string|int $month, string|int $year): self;
+
+    /**
+     * Returns the 4DBC, CVC, CVV code for the credit card
+     */
+    public function getSecurityCode(): string;
+
+    public function setSecurityCode(string $securityCode): self;
 }
