827ab458b2f82510430a66dc3827b8914c41633f
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-12-22 14:38:41 -0500

    updates

--- src/SonsOfPHP/Bard/src/Console/Command/CopyCommand.php
+++ src/SonsOfPHP/Bard/src/Console/Command/CopyCommand.php
@@ -25,6 +25,7 @@ final class CopyCommand extends AbstractCommand
             ->setDescription('Copies a file to each package')
             ->addOption('dry-run', null, InputOption::VALUE_NONE, 'Dry Run (Do not make any changes)')
             ->addArgument('source', InputArgument::REQUIRED, 'Source file to copy')
+            ->addArgument('package', InputArgument::OPTIONAL, 'Which package?')
             ->setHelp(
                 <<<'HELP'
 The <info>copy</info> command will copy whatever file you give it to all the other
@@ -55,6 +56,14 @@ HELP
         // ---
         $bardJsonFile = new JsonFile($input->getOption('working-dir') . '/bard.json');
         foreach ($bardJsonFile->getSection('packages') as $pkg) {
+            $pkgComposerFile     = realpath($input->getOption('working-dir') . '/' . $pkg['path'] . '/composer.json');
+            $pkgComposerJsonFile = new JsonFile($pkgComposerFile);
+            $pkgName             = $pkgComposerJsonFile->getSection('name');
+
+            if (null !== $input->getArgument('package') && $pkgName !== $input->getArgument('package')) {
+                continue;
+            }
+
             $process = new Process(['cp', $sourceFile, $pkg['path']]);
             $io->text($process->getCommandLine());
             if (!$isDryRun) {
