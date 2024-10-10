c79be0db54dfc3283a2322ca7dea448347d84515
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2024-10-09 23:21:13 -0400

    meh

--- rector.php
+++ rector.php
@@ -1,30 +1,7 @@
 <?php
 
 declare(strict_types=1);
-
-use Rector\CodeQuality\Rector\Assign\CombinedAssignRector;
-use Rector\CodeQuality\Rector\For_\ForRepeatedCountToOwnVariableRector;
-use Rector\CodeQuality\Rector\FuncCall\ArrayMergeOfNonArraysToSimpleArrayRector;
-use Rector\CodeQuality\Rector\Identical\BooleanNotIdenticalToNotIdenticalRector;
-use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
-use Rector\CodeQuality\Rector\If_\CombineIfRector;
-use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
-use Rector\CodeQuality\Rector\NullsafeMethodCall\CleanupUnneededNullsafeOperatorRector;
-use Rector\CodeQuality\Rector\Ternary\ArrayKeyExistsTernaryThenValueToCoalescingRector;
 use Rector\Config\RectorConfig;
-use Rector\Php82\Rector\FuncCall\Utf8DecodeEncodeToMbConvertEncodingRector;
-use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\AnnotationWithValueToAttributeRector;
-use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\CoversAnnotationWithValueToAttributeRector;
-use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\TicketAnnotationToAttributeRector;
-use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DataProviderAnnotationToAttributeRector;
-use Rector\PHPUnit\CodeQuality\Rector\Class_\YieldDataProviderRector;
-use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertCompareToSpecificMethodRector;
-use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertComparisonToSpecificMethodRector;
-use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertEqualsToSameRector;
-use Rector\PHPUnit\PHPUnit100\Rector\Class_\PublicDataProviderClassMethodRector;
-use Rector\PHPUnit\PHPUnit100\Rector\Class_\StaticDataProviderClassMethodRector;
-use Rector\PHPUnit\PHPUnit50\Rector\StaticCall\GetMockRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -58,27 +35,27 @@ return RectorConfig::configure()
     )
     ->withRules([
         // Generic
-        AddVoidReturnTypeWhereNoReturnRector::class,
-        ArrayKeyExistsTernaryThenValueToCoalescingRector::class,
-        ArrayMergeOfNonArraysToSimpleArrayRector::class,
-        BooleanNotIdenticalToNotIdenticalRector::class,
-        CleanupUnneededNullsafeOperatorRector::class,
-        CombineIfRector::class,
-        CombinedAssignRector::class,
-        ExplicitBoolCompareRector::class,
-        FlipTypeControlToUseExclusiveTypeRector::class,
-        ForRepeatedCountToOwnVariableRector::class,
-        Utf8DecodeEncodeToMbConvertEncodingRector::class,
+        //AddVoidReturnTypeWhereNoReturnRector::class,
+        //ArrayKeyExistsTernaryThenValueToCoalescingRector::class,
+        //ArrayMergeOfNonArraysToSimpleArrayRector::class,
+        //BooleanNotIdenticalToNotIdenticalRector::class,
+        //CleanupUnneededNullsafeOperatorRector::class,
+        //CombineIfRector::class,
+        //CombinedAssignRector::class,
+        //ExplicitBoolCompareRector::class,
+        //FlipTypeControlToUseExclusiveTypeRector::class,
+        //ForRepeatedCountToOwnVariableRector::class,
+        //Utf8DecodeEncodeToMbConvertEncodingRector::class,
         // PHPUnit Rules
-        AnnotationWithValueToAttributeRector::class,
-        AssertCompareToSpecificMethodRector::class,
-        AssertComparisonToSpecificMethodRector::class,
-        AssertEqualsToSameRector::class,
-        CoversAnnotationWithValueToAttributeRector::class,
-        DataProviderAnnotationToAttributeRector::class,
-        GetMockRector::class,
-        PublicDataProviderClassMethodRector::class,
-        StaticDataProviderClassMethodRector::class,
-        TicketAnnotationToAttributeRector::class,
-        YieldDataProviderRector::class,
+        //AnnotationWithValueToAttributeRector::class,
+        //AssertCompareToSpecificMethodRector::class,
+        //AssertComparisonToSpecificMethodRector::class,
+        //AssertEqualsToSameRector::class,
+        //CoversAnnotationWithValueToAttributeRector::class,
+        //DataProviderAnnotationToAttributeRector::class,
+        //GetMockRector::class,
+        //PublicDataProviderClassMethodRector::class,
+        //StaticDataProviderClassMethodRector::class,
+        //TicketAnnotationToAttributeRector::class,
+        //YieldDataProviderRector::class,
     ]);
