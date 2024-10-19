45ff6737d1e389566e32c0e416f5fabd60a32ba0
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2024-10-19 17:24:41 -0400

    updated tests

--- src/SonsOfPHP/Bard/Tests/Worker/File/Bard/AddPackageWorkerTest.php
+++ src/SonsOfPHP/Bard/Tests/Worker/File/Bard/AddPackageWorkerTest.php
@@ -6,7 +6,9 @@ namespace SonsOfPHP\Bard\Tests\File\Bard;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use SonsOfPHP\Bard\JsonFileInterface;
 use SonsOfPHP\Bard\Worker\File\Bard\AddPackageWorker;
 use SonsOfPHP\Bard\Worker\WorkerInterface;
 
@@ -14,13 +16,53 @@ use SonsOfPHP\Bard\Worker\WorkerInterface;
 #[CoversClass(AddPackageWorker::class)]
 final class AddPackageWorkerTest extends TestCase
 {
-    public function testItImplementsCorrectInterface(): void
+    private AddPackageWorker $worker;
+
+    private JsonFileInterface&MockObject $jsonFile;
+
+    protected function setUp(): void
     {
-        $worker = new AddPackageWorker([
+        $this->worker = new AddPackageWorker([
             'path'       => 'src/test',
             'repository' => 'git@github.com:vendor/repo.git',
         ]);
 
-        $this->assertInstanceOf(WorkerInterface::class, $worker);
+        $this->jsonFile = $this->createMock(JsonFileInterface::class);
+    }
+
+    public function testItImplementsCorrectInterface(): void
+    {
+        $this->assertInstanceOf(WorkerInterface::class, $this->worker);
+    }
+
+    public function testItWillThrowExceptionWhenPackageAtSamePathExists(): void
+    {
+        $this->jsonFile->method('getSection')->willReturn([
+            [
+                'path'       => 'src/test',
+                'repository' => 'git@github.com:vendor/repo.git',
+            ],
+        ]);
+
+        $this->expectException('Exception');
+        $this->worker->apply($this->jsonFile);
+    }
+
+    public function testItWillAddNewPackage(): void
+    {
+        $this->jsonFile->expects($this->once())->method('setSection')
+            ->with(
+                'packages',
+                $this->callback(function ($packages): true {
+                    $this->assertSame([[
+                        'path'       => 'src/test',
+                        'repository' => 'git@github.com:vendor/repo.git',
+                    ]], $packages);
+                    return true;
+                })
+            )
+        ;
+
+        $this->worker->apply($this->jsonFile);
     }
 }
